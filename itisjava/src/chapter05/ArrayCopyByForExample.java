3f15f2ef62638b2917e41671338328848d86dbeb
jieuncho2 <jieunchoi.developer@gmail.com>
2020-09-05 21:17:53 +0900

    chapter5 - 배열 타입 3

---
+++ itisjava/src/chapter05/ArrayCopyByForExample.java
@@ -0,0 +1,20 @@
+package chapter05;
+
+public class ArrayCopyByForExample {
+
+	public static void main(String[] args) {
+		int[] oldIntArray = {1, 2 , 3};
+		int[] newIntArray = new int[5];
+		
+		for(int i=0; i<oldIntArray.length; i++) {
+			newIntArray[i] = oldIntArray[i];
+		}
+		
+		for(int i=0; i<newIntArray[i]; i++) {
+			System.out.println(newIntArray[i] + ", ");
+		}
+				
+
+	}
+
+}
